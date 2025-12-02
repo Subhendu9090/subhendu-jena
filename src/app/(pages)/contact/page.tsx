4e131af39e6df4a68f5b76b5c9bf0c548dbe960d
“subhendu <subhendujena38@gmail.com>
2025-12-02 11:35:35 +0530

    change

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -5,7 +5,6 @@ import {
   User,
   MessageSquare,
   Pencil,
-  Phone,
   MapPin,
   Github,
   Linkedin,
--- src/components/Home/AboutNextProject.tsx
+++ src/components/Home/AboutNextProject.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { Handshake, Mail, MessageCircle } from "lucide-react";
+import { Handshake, Mail } from "lucide-react";
 import React, { useEffect, useState } from "react";
 
 function AboutNextProject() {
